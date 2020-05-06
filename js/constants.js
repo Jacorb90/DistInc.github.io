2c41a9b8199264c182ebb014e29bff7c3b14d460
Jacorb90 <39597610+Jacorb90@users.noreply.github.com>
2020-05-06 18:03:34 -0400

    α1.3.4
    Moved the save button to a new options menu

--- js/constants.js
+++ js/constants.js
@@ -154,6 +154,7 @@ const LAYER_SC = {
 
 const TABBTN_SHOWN = {
 	main: function() { return true },
+	options: function() { return true },
 	achievements: function() { return true },
 	rockets: function() { return (tmp.rockets ? (tmp.rockets.canRocket||player.rockets.gt(0)||player.rf.gt(0)) : false) },
 	auto: function() { return player.automation.unl },
