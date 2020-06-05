32d413fbecf1c6ed8c925c3a4b66eb92fb67ce71
Jacorb90 <39597610+Jacorb90@users.noreply.github.com>
2020-06-05 10:21:12 -0400

    Quick Bug Fix II

--- js/temp/ranks.js
+++ js/temp/ranks.js
@@ -9,12 +9,12 @@ function updateTempRanks() {
 	if (tmp.modes.hard.active && player.rank<3) tmp.ranks.bc = tmp.ranks.bc.times(2)
 	if (tmp.inf) if (tmp.inf.stadium.active("spaceon", 5)||tmp.inf.stadium.active("solaris", 6)) tmp.ranks.bc = tmp.ranks.bc.times(1e10)
 	tmp.ranks.req = new ExpantaNum(tmp.ranks.bc).times(ExpantaNum.pow(2, player.rank.div(tmp.ranks.fp).max(1).sub(1).pow(2)))
-	tmp.ranks.bulk = player.distance.div(tmp.ranks.bc).logBase(2).sqrt().plus(1).times(tmp.ranks.fp).plus(1)
+	tmp.ranks.bulk = player.distance.div(tmp.ranks.bc).logBase(2).sqrt().plus(1).times(tmp.ranks.fp).plus(1).round()
 	if (tmp.scaling.active("rank", player.rank.max(tmp.ranks.bulk), "scaled")) {
 		let power = tmp.scalingPower.scaled.rank
 		let exp = ExpantaNum.pow(2, power)
 		tmp.ranks.req = new ExpantaNum(tmp.ranks.bc).times(ExpantaNum.pow(2, (player.rank.pow(exp).div(tmp.scalings.scaled.rank.pow(exp.sub(1)))).div(tmp.ranks.fp).sub(1).pow(2)))
-		tmp.ranks.bulk = player.distance.div(tmp.ranks.bc).max(1).logBase(2).sqrt().plus(1).times(tmp.ranks.fp).times(tmp.scalings.scaled.rank.pow(exp.sub(1))).pow(exp.pow(-1)).plus(1)
+		tmp.ranks.bulk = player.distance.div(tmp.ranks.bc).max(1).logBase(2).sqrt().plus(1).times(tmp.ranks.fp).times(tmp.scalings.scaled.rank.pow(exp.sub(1))).pow(exp.pow(-1)).plus(1).floor()
 	}
 	if (tmp.scaling.active("rank", player.rank.max(tmp.ranks.bulk), "superscaled")) {
 		let power2 = tmp.scalingPower.superscaled.rank
@@ -22,7 +22,7 @@ function updateTempRanks() {
 		let power = tmp.scalingPower.scaled.rank
 		let exp = ExpantaNum.pow(2, power)
 		tmp.ranks.req = new ExpantaNum(tmp.ranks.bc).times(ExpantaNum.pow(2, ((player.rank.pow(exp2).div(tmp.scalings.superscaled.rank.pow(exp2.sub(1)))).pow(exp).div(tmp.scalings.scaled.rank.pow(exp.sub(1)))).div(tmp.ranks.fp).sub(1).pow(2)))
-		tmp.ranks.bulk = player.distance.div(tmp.ranks.bc).max(1).logBase(2).sqrt().plus(1).times(tmp.ranks.fp).times(tmp.scalings.scaled.rank.pow(exp.sub(1))).pow(exp.pow(-1)).times(tmp.scalings.superscaled.rank.pow(exp2.sub(1))).pow(exp2.pow(-1)).add(1)
+		tmp.ranks.bulk = player.distance.div(tmp.ranks.bc).max(1).logBase(2).sqrt().plus(1).times(tmp.ranks.fp).times(tmp.scalings.scaled.rank.pow(exp.sub(1))).pow(exp.pow(-1)).times(tmp.scalings.superscaled.rank.pow(exp2.sub(1))).pow(exp2.pow(-1)).add(1).floor()
 	}
 	
 	if (tmp.scaling.active("rank", player.rank.max(tmp.ranks.bulk), "hyper")) {
@@ -33,7 +33,7 @@ function updateTempRanks() {
 		let power = tmp.scalingPower.scaled.rank
 		let exp = ExpantaNum.pow(2, power)
 		tmp.ranks.req = new ExpantaNum(tmp.ranks.bc).times(ExpantaNum.pow(2, ((ExpantaNum.pow(base3, player.rank.sub(tmp.scalings.hyper.rank)).times(tmp.scalings.hyper.rank).pow(exp2).div(tmp.scalings.superscaled.rank.pow(exp2.sub(1)))).pow(exp).div(tmp.scalings.scaled.rank.pow(exp.sub(1)))).div(tmp.ranks.fp).sub(1).pow(2)))
-		tmp.ranks.bulk = player.distance.div(tmp.ranks.bc).max(1).logBase(2).sqrt().plus(1).times(tmp.ranks.fp).times(tmp.scalings.scaled.rank.pow(exp.sub(1))).pow(exp.pow(-1)).times(tmp.scalings.superscaled.rank.pow(exp2.sub(1))).pow(exp2.pow(-1)).div(tmp.scalings.hyper.rank).max(1).logBase(base3).add(tmp.scalings.hyper.rank).add(1)
+		tmp.ranks.bulk = player.distance.div(tmp.ranks.bc).max(1).logBase(2).sqrt().plus(1).times(tmp.ranks.fp).times(tmp.scalings.scaled.rank.pow(exp.sub(1))).pow(exp.pow(-1)).times(tmp.scalings.superscaled.rank.pow(exp2.sub(1))).pow(exp2.pow(-1)).div(tmp.scalings.hyper.rank).max(1).logBase(base3).add(tmp.scalings.hyper.rank).add(1).floor()
 	}
 	
 	if (tmp.ranks.bulk.lt(tmp.ranks.fp.plus(1))) tmp.ranks.bulk = tmp.ranks.bulk.max(tmp.ranks.fp.plus(1))
