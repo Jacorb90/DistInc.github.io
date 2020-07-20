e1407f811de51405423eb82599ed7cc38340c8cf
Jacorb90 <39597610+Jacorb90@users.noreply.github.com>
2020-07-19 22:12:47 -0400

    v1.6 RE Part 1
    - Moved functions/tr.js to temp/tr.js
    - Renamed "temp" folder to "main"

--- js/main/tr.js
+++ js/main/tr.js
@@ -28,3 +28,15 @@ function updateTempTR() {
 			buyTRUpg(i);
 		};
 }
+
+function reverseTime(force = false) {
+	if (!player.tr.unl) return;
+	player.tr.active = !player.tr.active;
+}
+
+function buyTRUpg(n) {
+	if (player.tr.upgrades.includes(n)) return;
+	if (player.tr.cubes.lt(TR_UPGS[n].cost)) return;
+	player.tr.cubes = player.tr.cubes.sub(TR_UPGS[n].cost);
+	player.tr.upgrades.push(n);
+}
