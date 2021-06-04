3ca3b2a7d7af502b388f82a9acae8dc84216d408
unsoftcapped3 <75136164+unsoftcapped3@users.noreply.github.com>
2021-06-04 14:54:27 -0700

    Update mode.js

--- js/main/mode.js
+++ js/main/mode.js
@@ -1,11 +1,1 @@
-const name = "test"
-let modName = "The ??? Incremental"
 
-// use if(modeActive(name)){stuff} to do stuff
-// go to line 265 in main.html and change the text to your mode's name
-
-
-
-
-
-document.title=modName
